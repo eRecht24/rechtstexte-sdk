651227c0305f1cccd23323ecf0879d6e115f6f8d
Malte Müns <m.muens@muensmedia.de>
2022-03-02 11:56:54 +0100

    [TASK] Test getLastErrorMessage() with language parameter

--- tests/ApiHandlerTest.php
+++ tests/ApiHandlerTest.php
@@ -5,6 +5,7 @@ namespace eRecht24\RechtstexteSDK\Tests;
 
 use eRecht24\RechtstexteSDK\ApiHandler;
 use eRecht24\RechtstexteSDK\Exceptions\Exception as ERecht24Exception;
+use eRecht24\RechtstexteSDK\Helper\Language as Lang;
 use eRecht24\RechtstexteSDK\Model\Response;
 use PHPUnit\Framework\TestCase;
 
@@ -25,5 +26,6 @@ final class ApiHandlerTest extends TestCase
         // getting client list without login should fail with 401 status code
         $this->assertEquals($client->getLastErrorCode(), 401);
         $this->assertEquals($client->getLastErrorMessage(), "Invalid key for the eRecht24 API.");
+        $this->assertEquals($client->getLastErrorMessage(Lang::DE_DE), "Ungültiger Schlüssel für die eRecht24 API.");
     }
 }
